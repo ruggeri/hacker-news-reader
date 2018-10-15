8d11b66d82ee6b1b7a0e58f7d443a0861cb84916
Ned Ruggeri <ruggeri@self-loop.com>
2018-10-15 16:22:46 -0700

    add comments for Config and DatabaseConnection

--- lib/config.rb
+++ lib/config.rb
@@ -1,8 +1,12 @@
 module HackerNewsReader; end
 
 module HackerNewsReader::Config
+  # Useful for debugging. Will print more info about what the script is
+  # doing.
   FETCH_LOGGING = false
+  # I don't want to send an email with the stories until a critical mass
+  # of interesting stories builds up.
   MIN_EMAIL_LENGTH = 10
-  # Wait 1hr before refreshing a story.
+  # Wait 1hr before refreshing a story. Limits the number of API calls.
   SECONDS_BEFORE_REFRESH = 60 * 60.0
 end
--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -4,9 +4,10 @@ module HackerNewsReader; end
 
 # Methods that build the underlying db connection.
 module HackerNewsReader::DatabaseConnection
+  # To use these scripts, you'll have to `createdb hacker_news`.
   DB_NAME = "hacker_news"
 
-  # Gets a db connection.
+  # Gets a Postgres db connection.
   def self.get_db_connection
     return @db if @db
 
@@ -19,6 +20,19 @@ module HackerNewsReader::DatabaseConnection
     @db
   end
 
+  # This creates a Postgres type to mark whether we are interested in a
+  # story. The possible values are 'UNMARKED', 'IGNORED', and
+  # 'INTERESTING'.
+  #
+  # Postgres will be smart enough to use just two bits per row to store
+  # this value: it won't store the whole string. Plus, Postgres will
+  # tell us if we try to set the marking to 'BLAH_BLAH_BLAH' or
+  # something.
+  #
+  # This just creates the type. We'll use it when creating the table.
+  #
+  # I need to use DO/END because I'll also be using an IF NOT. I'm
+  # checking in case I've already defined the marking type.
   def self.maybe_add_marked_enum(db)
     db.exec <<-SQL
       DO $$
@@ -34,19 +48,33 @@ module HackerNewsReader::DatabaseConnection
     SQL
   end
 
+  # Creates the items table if this hasn't been done yet. This will
+  # store the pulled hacker news stories.
   def self.maybe_add_items_table(db)
     db.exec <<-SQL
-      CREATE TABLE IF NOT EXISTS items (
-        id INTEGER NOT NULL UNIQUE,
-        score INTEGER NOT NULL,
-        title VARCHAR(1024) NOT NULL,
-        url VARCHAR(1024),
-        author VARCHAR(1024) NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-        pulled_at TIMESTAMP WITH TIME ZONE NOT NULL,
-        did_email BOOLEAN NOT NULL,
-        marking marking NOT NULL
-      );
+      DO $$
+      BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'items') THEN
+          CREATE TABLE items (
+            -- Hacker news gives us an item id.
+            id INTEGER NOT NULL UNIQUE,
+            -- The number of upvotes.
+            score INTEGER NOT NULL,
+            title VARCHAR(1024) NOT NULL,
+            -- Sometimes there is no URL if this is an "Ask HN" post.
+            url VARCHAR(1024),
+            author VARCHAR(1024) NOT NULL,
+            -- The time the story was authored at.
+            created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+            -- When have we most recently pulled the story.
+            pulled_at TIMESTAMP WITH TIME ZONE NOT NULL,
+            -- Did we already email ourselves this story?
+            did_email BOOLEAN NOT NULL,
+            -- Is the story interesting?
+            marking marking NOT NULL
+          );
+        END IF;
+      END$$;
     SQL
   end
 end
